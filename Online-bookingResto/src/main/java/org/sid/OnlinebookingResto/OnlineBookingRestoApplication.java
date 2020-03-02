6b7668f3335b680a71561384090108ef15d33ab5
rogeraime <rogerngnosse@gmail.com>
2020-03-02 16:48:55 +0100

    Set up the project with Spring Boot

---
+++ Online-bookingResto/src/main/java/org/sid/OnlinebookingResto/OnlineBookingRestoApplication.java
@@ -0,0 +1,13 @@
+package org.sid.OnlinebookingResto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookingRestoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookingRestoApplication.class, args);
+	}
+
+}
